8d8d1de50763295249a855c953fd5971cd816978
User <user01@gmail.com>
2025-08-29 10:24:33 +0900

    1st

--- src/main/java/Attendance.java
+++ src/main/java/Attendance.java
@@ -3,15 +3,15 @@ import java.util.*;
 
 public class Attendance {
 
-    static class Node {
-        String w;
-        String wk;
-    }
+    public static final int[] WeekDayPoint = {1, 1, 3, 1, 1, 2, 2};
+    public static final int grade1_point=50;
+    public static final int grade2_point=30;
+    public static final String[] gradeTitle={"NORMAL","GOLD","SILVER"};
 
     static Map<String, Integer> playerIDList = new HashMap<>();
     static int idCnt = 0;
 
-    static int[][] dat = new int[100][100];
+    static int[][] playerAttendanceCount = new int[100][100];
     static int[] points = new int[100];
     static int[] grade = new int[100];
     static String[] playerName = new String[100];
@@ -19,56 +19,47 @@ public class Attendance {
     static int[] wed = new int[100];
     static int[] weeken = new int[100];
 
-    public static void checkingAttendance(String playerName, String attendWeekDay) {
+    public static Integer getPlayerID(String playerName) {
         if (!playerIDList.containsKey(playerName)) {
             playerIDList.put(playerName, ++idCnt);
             Attendance.playerName[idCnt] = playerName;
         }
-        int playerID = playerIDList.get(playerName);
-        
-        addAttendancePoint(attendWeekDay, playerID);
+        return playerIDList.get(playerName);
     }
 
-    private static void addAttendancePoint(String attendWeekDay, int playerID) {
+    private static void addAttendancePoint(int playerID, String attendWeekDay) {
         int addPoint = 0;
         int index = 0;
 
         switch (attendWeekDay) {
             case "monday":
                 index = 0;
-                addPoint++;
                 break;
             case "tuesday":
                 index = 1;
-                addPoint++;
                 break;
             case "wednesday":
                 index = 2;
-                addPoint += 3;
-                wed[playerID]++;
                 break;
             case "thursday":
                 index = 3;
-                addPoint++;
                 break;
             case "friday":
                 index = 4;
-                addPoint++;
                 break;
             case "saturday":
                 index = 5;
-                addPoint += 2;
-                weeken[playerID]++;
                 break;
             case "sunday":
                 index = 6;
-                addPoint += 2;
-                weeken[playerID]++;
                 break;
         }
 
-        dat[playerID][index]++;
-        points[playerID] += addPoint;
+
+        if (index == 2) wed[playerID]++;
+        if (index > 4) weeken[playerID]++;
+        playerAttendanceCount[playerID][index]++;
+        points[playerID] += WeekDayPoint[index];
     }
 
     public static void input() {
@@ -78,14 +69,10 @@ public class Attendance {
                 if (line == null) break;
                 String[] parts = line.split("\\s+");
                 if (parts.length == 2) {
-                    checkingAttendance(parts[0], parts[1]);
+                    addAttendancePoint(getPlayerID(parts[0]), parts[1]);
                 }
             }
 
-
-            addBonusPoint();
-            ratingPlayer();
-            firePlayer();
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -106,9 +93,9 @@ public class Attendance {
     private static void ratingPlayer() {
         for (int i = 1; i <= idCnt; i++) {
 
-            if (points[i] >= 50) {
+            if (points[i] >= grade1_point) {
                 grade[i] = 1;
-            } else if (points[i] >= 30) {
+            } else if (points[i] >= grade2_point) {
                 grade[i] = 2;
             } else {
                 grade[i] = 0;
@@ -118,23 +105,17 @@ public class Attendance {
             System.out.print("POINT : " + points[i] + ", ");
             System.out.print("GRADE : ");
 
-            if (grade[i] == 1) {
-                System.out.println("GOLD");
-            } else if (grade[i] == 2) {
-                System.out.println("SILVER");
-            } else {
-                System.out.println("NORMAL");
-            }
+            System.out.print(gradeTitle[grade[i]]);
+
         }
     }
 
     private static void addBonusPoint() {
         for (int i = 1; i <= idCnt; i++) {
-            if (dat[i][2] > 9) {
+            if (playerAttendanceCount[i][2] > 9) {
                 points[i] += 10;
             }
-
-            if (dat[i][5] + dat[i][6] > 9) {
+            if (playerAttendanceCount[i][5] + playerAttendanceCount[i][6] > 9) {
                 points[i] += 10;
             }
         }
@@ -142,5 +123,13 @@ public class Attendance {
 
     public static void main(String[] args) {
         input();
+        addBonusPoint();
+        ratingPlayer();
+        firePlayer();
+    }
+
+    static class Node {
+        String w;
+        String wk;
     }
 }
